ac1197a85b30a746032460e27b8ad65db5d01341
RC816 <caiyitian816@126.com>
2018-03-09 18:13:33 +0800

    UPDATE

--- guizhou/js/login.js
+++ guizhou/js/login.js
@@ -1,5 +1,39 @@
+var code; //在全局 定义验证码
+
+function createCode() {
+    code = new Array();
+    var codeLength = 4; //验证码的长度
+    var checkCode = document.getElementById("checkCode");
+    checkCode.value = "";
+    var selectChar = new Array(2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+    for (var i = 0; i < codeLength; i++) {
+        var charIndex = Math.floor(Math.random() * 32);
+        code += selectChar[charIndex];
+    }
+    if (code.length != codeLength) {
+        createCode();
+    }
+    checkCode.value = code;
+}
+
+function validate() {
+    var inputCode = document.getElementById("code").value.toUpperCase();
+    if (inputCode.length <= 0) {
+        alert("请输入验证码！");
+        return false;
+    } else if (inputCode != code) {
+        alert("验证码输入错误！");
+        createCode();
+        return false;
+    } else {
+        alert("成功！");
+        return true;
+    }
+}
+
 $(function () {
     $(document).on('click', '#login', function () {
+        validate();
         var $userName = $('#user-name'),
             $pwd = $('#user-pwd');
         if (!$userName.val()) {
@@ -29,36 +63,25 @@ $(function () {
         CommonTools.getData(params);
     });
 
-    $(document).on('focus', '.normal-input', function () {
-        $('.common-error-tips').hide();
-    });
-
-    $(document).on('keydown', '.normal-input', function (e) {
-        var e = event || window.event || arguments.callee.caller.arguments[0];
-        if (e && e.keyCode == 13) {
-            $('#login').trigger('click');
-        }
-    });
-    $(document).on('keydown', '.normal-input', function (e) {
-        var e = event || window.event || arguments.callee.caller.arguments[0];
-        if (e && e.keyCode == 13) {
-            $('#login').trigger('click');
-        }
-    });
-
-    $("#getData").click(function () {
-        var params={
-            url:'api/apptoken/getuser',
-            tokenFlag:true,
-            sCallback:function(res) {
-                console.log(res);
-            }
-        };
-        CommonTools.getData(params);
-    });
-
     $("#logout").click(function () {
         CommonTools.deleteLocalStorage('token');
     });
-
+    createCode();
 });
+
+/*
+$(document).on('focus', '.normal-input', function () {
+    $('.common-error-tips').hide();
+});
+$(document).on('keydown', '.normal-input', function (e) {
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    if (e && e.keyCode == 13) {
+        $('#login').trigger('click');
+    }
+});
+$(document).on('keydown', '.normal-input', function (e) {
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    if (e && e.keyCode == 13) {
+        $('#login').trigger('click');
+    }
+});*/
