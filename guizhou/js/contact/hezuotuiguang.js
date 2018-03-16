e5eac0af07a037ce06b72572b1d4bf279cc60f59
shaossss <969348836@qq.com>
2018-03-16 18:02:36 +0800

    update

---
+++ guizhou/js/contact/hezuotuiguang.js
@@ -0,0 +1,40 @@
+var hezuotuiguangViewModel = function () {
+    var self = this;
+    self.link1 = ko.observableArray([]);
+    self.link2 = ko.observableArray([]);
+    self.link = ko.observableArray([ self.link1,  self.link2]);
+}
+
+var hztgModel = new hezuotuiguangViewModel();
+
+var getLink = new Promise(function (resolve,reject) {
+    $.get("http://192.168.0.191/home/content/cooperation", function (returnData) {
+        if (returnData.code && returnData.code == '200') {
+            if (returnData.data && returnData.data.list && returnData.data.list.length > 0) {
+                var mappingList = {
+                    'img': {
+                        create: function (options) {
+                            return 'http://192.168.0.191'+options.data;
+                        }
+                    }
+                }
+                for (var i = 0; i <= returnData.data.list.length; i++) {
+                    ko.mapping.fromJS(returnData.data.list[i],mappingList, hztgModel.link()[i]);
+                }
+            }
+            resolve("success");
+        }
+        else {
+            reject("failed");
+            console.log("数据获取有错误");
+        }
+    });
+});
+
+
+$(function () {
+    getLink.then(function () {
+        ko.applyBindings(hztgModel);
+        CommonTools.getAutoHeight($('#auto-content'));
+    });
+});
