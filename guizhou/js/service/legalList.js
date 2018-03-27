1f5d9b6df25a4e26c1fe4e5083497b15a21fabfa
shaossss <969348836@qq.com>
2018-03-27 16:19:50 +0800

    update

--- guizhou/js/service/legalList.js
+++ guizhou/js/service/legalList.js
@@ -3,7 +3,7 @@ var categoryName = decodeURIComponent(CommonTools.getQueryVariable("categoryName
 
 var getLegalList = function () {
     var pageInfo = {
-        limit: 1,
+        limit: 5,
         page: llModel.currentPage(),
     };
     $.get(g_restUrl+'home/content/servicesscate/category/'+categoryId,pageInfo,function (returnData) {
